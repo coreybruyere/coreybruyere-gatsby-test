3276be13ef36ee8d431e472b568f536a1c4a666d
Corey Bruyere <coreybruyere@ymail.com>
2018-10-09 20:48:14 -0700

    WTF

---
+++ src/templates/work.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Helmet from 'react-helmet'
+import config from '../utils/siteConfig'
+import Layout from '../components/Layout'
+import Hero from '../components/Hero'
+import Container from '../components/Container'
+import PageBody from '../components/PageBody'
+import TagList from '../components/TagList'
+import PostLinks from '../components/PostLinks'
+import PostDate from '../components/PostDate'
+import SEO from '../components/SEO'
+
+const WorkTemplate = ({ data, pageContext }) => {
+  const {
+    title,
+    slug,
+    heroImage,
+    body,
+    publishDate,
+    tags,
+  } = data.contentfulWork
+  const workNode = data.contentfulWork
+
+  const previous = pageContext.prev
+  const next = pageContext.next
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>{`${title} - ${config.siteTitle}`}</title>
+      </Helmet>
+      <SEO pagePath={slug} postNode={workNode} postSEO />
+
+      <Hero title={title} image={heroImage} height={'50vh'} />
+
+      <Container>
+        WORK!
+        {tags && <TagList tags={tags} />}
+        <PostDate date={publishDate} />
+        <PageBody body={body} />
+        <PostLinks previous={previous} next={next} />
+      </Container>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulWork(slug: { eq: $slug }) {
+      title
+      slug
+      metaDescription {
+        internal {
+          content
+        }
+      }
+      publishDate(formatString: "MMMM DD, YYYY")
+      publishDateISO: publishDate(formatString: "YYYY-MM-DD")
+      heroImage {
+        title
+        fluid(maxWidth: 1800) {
+          ...GatsbyContentfulFluid_withWebp_noBase64
+        }
+        ogimg: resize(width: 1800) {
+          src
+          width
+          height
+        }
+      }
+      body {
+        childMarkdownRemark {
+          html
+          excerpt(pruneLength: 320)
+        }
+      }
+    }
+  }
+`
+
+export default WorkTemplate
